f27cdb2d395307583c22ea95500ad307a12d816c
Janssen Batista <batistajanssen.dev@gmail.com>
2024-09-11 17:50:01 -0300

    feat: add SaldoInsuficienteException class

---
+++ src/main/java/dev/janssenbatista/exceptions/SaldoInsuficienteException.java
@@ -0,0 +1,7 @@
+package dev.janssenbatista.exceptions;
+
+public class SaldoInsuficienteException extends RuntimeException {
+    public SaldoInsuficienteException(String message) {
+        super(message);
+    }
+}

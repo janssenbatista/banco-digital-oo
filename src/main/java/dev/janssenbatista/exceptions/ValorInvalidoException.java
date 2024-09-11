442c4ec1582176bce5655af6435fad121d41bf2c
Janssen Batista <batistajanssen.dev@gmail.com>
2024-09-11 17:51:00 -0300

    feat: add ValorInvalidoException class

---
+++ src/main/java/dev/janssenbatista/exceptions/ValorInvalidoException.java
@@ -0,0 +1,7 @@
+package dev.janssenbatista.exceptions;
+
+public class ValorInvalidoException extends RuntimeException {
+    public ValorInvalidoException(String message) {
+        super(message);
+    }
+}
